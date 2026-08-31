3d192df4f9637c204aef793ac2a6565eb0011d5c
Lausabogal22 <lausabogal22@gmail.com>
2026-08-31 17:16:32 +0000

    .

---
+++ App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main (String [] args){
+        
+        double [] array = {-1.5, 2.5, 3.5, 4.1, 5.8};
+
+        for (int i =0 ; i < array.length ; i++){
+            System.out.print(array[i] + " ");
+        }
+
+        String [] names =  new String [5];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter five names");
+
+        for(int i =0 ; i < 5 ; i++){
+            names[i] = input.nextLine();
+        }
+
+        System.out.println("The names that you entenreda are: ");
+        for(int i =0 ; i < 5 ; i++){
+            System.out.print(names[i] + " ");
+        }
+    }
+}
+
